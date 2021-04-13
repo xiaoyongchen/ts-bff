3aa1c24a7ac0ed83ffb9e6a09012aa4c4afed462
陈小勇 <xiaoyong.chen@zhangmen.com>
2021-04-13 11:39:46 +0800

    fix: 动态路由

--- example/router.ts
+++ example/router.ts
@@ -6,7 +6,14 @@ const router = new Router();
 
 router.get('/list', context => {
   context.body = [1, 2, 3];
+});
+router.get('/list/:id', content => {
+  content.body = {
+    id: content.params.id,
+    time: new Date()
+  }
 })
+
 
 // allowedMethods 如果请求报错会显示请求方式应该是哪些，对跨域有些提示
 app.use(router.routes()).use(router.allowedMethods());
